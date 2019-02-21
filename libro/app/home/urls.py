b587cd040099c28dba4485a50c470eef604d019f
Giancarlo <giancarlo.zevallos.92@gmail.com>
2019-02-21 00:59:35 -0500

    View lista

--- libro/app/home/urls.py
+++ libro/app/home/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name="home_app"
 
 urlpatterns = [
-    path('index', views.IndexView.as_view(), name='index')
+    path('index', views.IndexView.as_view(), name='index'),
+    path('libros', views.ListaLibros.as_view(), name='libros')
 ]
--- libro/app/home/views.py
+++ libro/app/home/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 from django.views.generic import (
     TemplateView,
+    ListView
 )
 
 class IndexView(TemplateView):
     template_name = "home/index.html"
+
+class ListaLibros(ListView):
+    template_name = "home/lista.html"
+    queryset = ["El quijote de la mancha", "Codigo Limpio", "La sombra del viento", "La casa Matusita"]
+    context_object_name = "libros"
